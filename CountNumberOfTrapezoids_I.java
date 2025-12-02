87a1d90b2573b45b69f3d8bbea4ac732cd70b0ed
Subhajyoti Prusty <70449522+subhajyoti-prusty@users.noreply.github.com>
2025-12-02 23:30:18 +0530

    3623

---
+++ CountNumberOfTrapezoids_I.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CountNumberOfTrapezoids_I {
+    public int countTrapezoids(int[][] points) {
+        long mod = 1000000007L;
+        Map<Integer, Long> mapy = new HashMap<>();
+        for (int[] row : points) {
+            mapy.put(row[1], mapy.getOrDefault(row[1], 0L) + 1);
+        }
+        List<Long> height = new ArrayList<>();
+        for (long val : mapy.values()) {
+            if (val >= 2) {
+                long ways = (val * (val - 1)) / 2;
+                height.add(ways % mod);
+            }
+        }
+        long ans = 0, sum = 0;
+        for (long val : height) {
+            ans = (ans + (val * sum) % mod) % mod;
+            sum = (sum + val) % mod;
+        }
+        return (int) ans;
+    }
+}
